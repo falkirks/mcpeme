7f8642c278705c44be1f9dbada810427c8ba597a
Noah Heyl <me@falkirks.com>
2018-04-05 17:32:22 -0700

    Fix description for SEO

--- config/env/default.js
+++ config/env/default.js
@@ -3,8 +3,8 @@
 module.exports = {
   app: {
     title: 'mcpe.me',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    description: 'Free hostnames for MCPE servers and Minecraft related things.',
+    keywords: 'minecraft,mcpe,hostnames,cname,server',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
